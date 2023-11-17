3cb516af433bb1fcefa08d0837f3e9b24ef752be
Milkias SEMEREAB <75955372+milkiaszerai@users.noreply.github.com>
2023-11-17 15:21:00 -0500

    Update solar_doomsday.py

--- challenges/level_1_solar_doomsday/solar_doomsday.py
+++ challenges/level_1_solar_doomsday/solar_doomsday.py
@@ -1,1 +1,29 @@
+import math
+
+def solution(area=0):
+  """
+    Creates list of square panels from given area
+
+    Args:
+        area (sq. yards): total area of solar panels in range [1, 1000000]. Default = 0 
+
+    Returns:
+        panels (list): list of the areas of the largest squares you could make out of those panels. 
+  """
+
+  # Initialize empty list to store the square panels
+  panels = []
+  area_ = area   # area_ = area remaining after each iteration
+  
+  while True:
+    panel = math.sqrt(area_)
+    panel = (int(panel))**2
+    panels.append(panel)
+    area_ = area - sum(panels) 
+    if area_ == 0:
+      break
+  return panels 
+
+if __name__ == "__main__":
+    solution()
 
