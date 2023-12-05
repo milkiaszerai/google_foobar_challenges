cdc8f57c30f529a2dc6cc4124476878b24ba447c
Milkias SEMEREAB <75955372+milkiaszerai@users.noreply.github.com>
2023-12-04 19:51:45 -0500

    Update solution.py

--- challenges/level_3_fuel-injection-perfection/solution.py
+++ challenges/level_3_fuel-injection-perfection/solution.py
@@ -1,1 +1,9 @@
-
+def solution(s):
+    ''' 
+    Program that counts how many salutes are exchanged during a typical walk along a hallway which is represented by a string. For example: "--->-><-><-->-" 
+    
+    Args: 
+        s: string of unkown length
+    Returns:
+        n(int): number of salutes
+    '''
