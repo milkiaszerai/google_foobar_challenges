fb1568bc95fb8f893e6d1d4a64f85f860df721e8
Milkias SEMEREAB <75955372+milkiaszerai@users.noreply.github.com>
2023-12-04 19:56:46 -0500

    Update solution.py

--- challenges/level_3_fuel-injection-perfection/solution.py
+++ challenges/level_3_fuel-injection-perfection/solution.py
@@ -8,6 +8,19 @@ def solution(n):
     Returns:
         res (int): minimum number of operations needed to transform the number of pellets to 1
 
-    Example:
-        
+    Examples:
+        solution(4) returns 2: 4 -> 2 -> 1
+        solution(15) returns 5: 15 -> 16 -> 8 -> 4 -> 2 -> 1
     '''
+    n = int(n)
+    res = 0
+
+    while(n!=1):
+        if(n%2==0):
+            n=n/2
+        elif((n==3) or ((n+1)&n) > ((n-1)&(n-2))):
+            n-=1
+        else:
+            n+=1
+        res+=1
+    return res
